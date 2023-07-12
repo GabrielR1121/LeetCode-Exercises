ce2c4c773ba1cfb0e95f3e987aed19eab76363e8
Gabriel Rodriguez <gr.garcia1121@gmail.com>
2023-07-11 21:02:16 -0400

    Optimized Exercise #1 in Java

--- 1. Two Sum/two_sum.java
+++ 1. Two Sum/two_sum.java
@@ -1,15 +1,16 @@
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.HashMap;
+import java.util.Map;
 
 public class two_sum {
     /**
      * Solved By: Gabriel E. Rodriguez Garcia
      * Date Completed: July 10, 2023
+     * Date Optimized: July 11, 2023
      * Exersise #1
      * Exercise Name: Two Sum
      * Difficulty: Easy
-     * Time Complexity: O(nlogn)
+     * Time Complexity: O(n)
      * Space Complexity: O(n)
      * Language Used: Java
      * Tags: Array, Hash Table
@@ -20,11 +21,8 @@ public class two_sum {
         // Creates sample target to use
         int target = 9;
 
-        // Stores the answer from the method
-        int[] answer = twoSum(nums, target);
-
         // Prints sample answer to the console
-        System.out.println(Arrays.toString(answer));
+        System.out.println(Arrays.toString(twoSum(nums, target)));
     }
 
     /**
@@ -37,44 +35,21 @@ public class two_sum {
      *         sum up to the target
      */
     public static int[] twoSum(int[] nums, int target) {
-        // Creates an array,with a size of 2, that will store the answer.
-        int answer[] = new int[2];
+        // Creates a hashmap in order to store the key value pair of the number with an
+        // index;
+        Map<Integer, Integer> numbers = new HashMap<>();
+        // Loops through the original array in order to store a number with its index in
+        // the hashmap
+        for (int i = 0; i < nums.length; i++)
+            // If the value exists in the hashmap
+            if (numbers.containsKey((target - nums[i])))
+                // Return a new array with their indexes
+                return new int[] { numbers.get(target - nums[i]), i };
+            else
+                // Else store the new value in the hashmap with the index
+                numbers.put(nums[i], i);
 
-        // Converts the nums array into an array list to be able to search for indexes
-        List<Integer> searchArr = Arrays.stream(nums).boxed().collect(Collectors.toList());
-
-        // Creates a copy of nums so that we can sort the copy for binary search
-        int sortedArr[] = nums;
-
-        // Indexes of both arrays
-        int index = 0;
-        int foundIndex = 0;
-
-        // Sorts the copy of nums.
-        Arrays.sort(sortedArr);
-
-        // Runs while found index is negative or we reach the end of the array.
-        do {
-
-            foundIndex = Arrays.binarySearch(sortedArr, (target - sortedArr[index]));
-
-            if (foundIndex >= 0)
-                break;
-
-        } while (sortedArr.length != index++);
-
-        // If the numbers are the same then find the first and last reference of their
-        // indexes so the indexes dont repeat.
-        if (sortedArr[index] == sortedArr[foundIndex]) {
-            answer[0] = searchArr.indexOf(sortedArr[index]);
-            answer[1] = searchArr.lastIndexOf(sortedArr[foundIndex]);
-            // Else just store their indexes
-        } else {
-
-            answer[0] = searchArr.indexOf(sortedArr[index]);
-            answer[1] = searchArr.indexOf(sortedArr[foundIndex]);
-        }
-        // Return the answer.
-        return answer;
+        // returns an empty array
+        return new int[] {};
     }
 }
