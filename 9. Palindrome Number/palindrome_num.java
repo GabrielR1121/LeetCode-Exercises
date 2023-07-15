5cd1b63638f83b755b443364015b9f804aa2fb80
Gabriel E. Rodriguez Garcia <gr.garcia1121@gmail.com>
2023-07-15 12:40:35 -0400

    Solved Exercise #9 in Java

--- 9. Palindrome Number/palindrome_num.java
+++ 9. Palindrome Number/palindrome_num.java
@@ -1,15 +1,29 @@
 public class palindrome_num {
-    public static void main(String[] args) {
+ /**
+     * Solved By: Gabriel E. Rodriguez Garcia
+     * Date Completed: March 26, 2022
+     * Exersise #9
+     * Exercise Name: Palindrome Number
+     * Difficulty: Easy
+     * Time Complexity: O(n)
+     * Space Complexity: O(n)
+     * Language Used: Java
+     * Tags: Math
+     */
 
+    public static void main(String[] args) {
+        //Sample String
+        int number = 121;
+        //Prints the result to the console.
+        System.out.println(isPalindrome(number));
     }
 
     /**
      * Checks if a number is a palindrome
-     * 
      * @param x A number
      * @return True or Flase depending on if the number is a palindrome or not
      */
-    public boolean isPalindrome(int x) {
+    public static boolean isPalindrome(int x) {
 
         // Stores the original number and converts it to a string.
         // We do this because String Builder has the method called reverse. Which
