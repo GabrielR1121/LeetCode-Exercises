911ce828424510f2c838434aa6214f1389328ab2
Gabriel E. Rodriguez Garcia <gr.garcia1121@gmail.com>
2023-07-25 17:21:22 -0400

    Solved Exercise #1929 in Python

---
+++ 1929. Concatenation of Array/concat_array.py
@@ -0,0 +1,25 @@
+"""
+     * Solved By: Gabriel E. Rodriguez Garcia
+     * Date Completed: July 23, 2023
+     * Exersise #1929
+     * Exercise Name: Concatination Of Array
+     * Difficulty: Easy
+     * Time Complexity: O(n)
+     * Space Complexity: O(n)
+     * Language Used: Python
+     * Tags: Array
+"""
+
+"""
+     * Doubles the contents of an array with the same values
+     * @param nums An array of numbers
+     * @return Returns a doubled array with the same values.
+"""
+def getConcatenation(nums):
+        return nums*2
+
+#Sample array
+nums =[1,2,1]
+
+#Prints the result to the console
+print(getConcatenation(nums))
