fa50a72b6d899c7b785c21101d7f03c6087b7836
Gabriel E. Rodriguez Garcia <gr.garcia1121@gmail.com>
2023-07-25 16:55:44 -0400

    Solved Exercise #557 in Python

---
+++ 557. Reverse Words in a String III/reverse_string_III.js
@@ -0,0 +1,32 @@
+/**
+     * Solved By: Gabriel E. Rodriguez Garcia
+     * Date Completed: July 23, 2023
+     * Exersise #557
+     * Exercise Name: Reverse String III
+     * Difficulty: Easy
+     * Time Complexity: O(n)
+     * Space Complexity: O(n)
+     * Language Used: JavaScript
+     * Tags: Two Pointer, String
+*/
+
+
+/**
+ * Reverses a sentence word for word
+ * @param {string} s A sentence to be reversed
+ * @return {string} The reversed sentence
+ */
+var reverseWords = function(s) {
+    //Splits the sentence into words
+    let words = s.split(" ");
+
+    //Temp variable to fild each word
+    let temp = [];
+
+    //Traverse the words and fild them individually 
+    for(let i=0; i<words.length; i++)
+        temp.push(words[i].split("").reverse().join(""));
+
+    //Returns the filpped sentence. 
+    return temp.join(" ");
+};
