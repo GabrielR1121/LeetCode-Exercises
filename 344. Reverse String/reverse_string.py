b0a60242936c04fa49feb8219bb2a90fc14a3be8
Gabriel E. Rodriguez Garcia <gr.garcia1121@gmail.com>
2023-07-25 21:21:33 -0400

    Solved Exercise #544 in Python

---
+++ 344. Reverse String/reverse_string.py
@@ -0,0 +1,25 @@
+"""
+     * Solved By: Gabriel E. Rodriguez Garcia
+     * Date Completed: July 23,2023
+     * Exersise #344
+     * Exercise Name: Reverse String
+     * Difficulty: Easy
+     * Time Complexity: O(n)
+     * Space Complexity: O(n)
+     * Language Used: Python
+     * Tags: Two Pointer, String
+"""
+
+"""
+     * Reverse a string in place. 
+     * @param s A string containg the words to reverse.
+"""
+def reverseString(s) -> None:
+     print(s.reverse())
+
+
+#Sample String
+sentence = "hello"
+
+#Reverses the string in pace
+reverseString(sentence)
